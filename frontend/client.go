2ed8f72fab417d505226d07dc5889aa8ba13b9fd
YfsBox <1027342142@qq.com>
2022-12-15 08:34:12 +0800

    feat(frontend): 实现client中的start和stop

--- frontend/client.go
+++ frontend/client.go
@@ -4,14 +4,46 @@ import (
 	"fmt"
 	"github.com/urfave/cli"
 	"log"
+	"net"
+	"time"
+)
+
+const (
+	PROTOCOL_FAMILY = "tcp"
 )
 
 type IdentifierResponse struct {
 }
 
-func sendIdentifierMsg(name string, id string, action string) (IdentifierResponse, error) {
+type Client struct {
+	Addr    string // 表示的是Server的地址
+	Conn    net.Conn
+	Timeout int64
+}
+
+var client Client
+
+func (c *Client) init(addr string, timeout int64) error {
+	var err error
+	c.Addr = addr
+	c.Timeout = timeout
+	c.Conn, err = net.DialTimeout(PROTOCOL_FAMILY, c.Addr, 4*time.Second)
+	if err != nil {
+		return fmt.Errorf("%v: Create client Conn error", err)
+	}
+	return nil
+}
+
+func (c *Client) sendIdentifierMsg(name string, id string, action string) (IdentifierResponse, error) {
 	idres := IdentifierResponse{}
 	return idres, nil
+}
+
+func (c *Client) stop() error {
+	if err := c.Conn.Close(); err != nil {
+		return fmt.Errorf("%v: close Conn of client error", err)
+	}
+	return nil
 }
 
 func DoIdentifier(context *cli.Context, action_type string) (IdentifierResponse, error) {
@@ -21,7 +53,7 @@ func DoIdentifier(context *cli.Context, action_type string) (IdentifierResponse,
 		return idres, fmt.Errorf("getMonitorIdentifier error %v", err)
 	}
 	log.Printf("The mname is %v and mid is %v\n", mname, mid)
-	idres, err = sendIdentifierMsg(mname, mid, action_type)
+	idres, err = client.sendIdentifierMsg(mname, mid, action_type)
 	if err != nil {
 		return idres, fmt.Errorf("SendIdentierMsg error %v", err)
 	}
